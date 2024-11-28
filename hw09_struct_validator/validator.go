93c836cecf822bcf709e1bbae409d83490a54b25
Anton Trufanov <anton.t1984@gmail.com>
2024-11-28 17:45:14 +0300

    HW9 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,85 @@
 package hw09structvalidator
 
-type ValidationError struct {
-	Field string
-	Err   error
-}
+import (
+	"errors"
+	"fmt"
+	"reflect"
 
-type ValidationErrors []ValidationError
-
-func (v ValidationErrors) Error() string {
-	panic("implement me")
-}
+	"github.com/antonT001/psychic-parakeet/hw09_struct_validator/constants"
+	"github.com/antonT001/psychic-parakeet/hw09_struct_validator/e"
+	"github.com/antonT001/psychic-parakeet/hw09_struct_validator/integer"
+	"github.com/antonT001/psychic-parakeet/hw09_struct_validator/str"
+)
 
 func Validate(v interface{}) error {
-	// Place your code here.
-	return nil
+	t := reflect.TypeOf(v)
+	switch t.Kind() {
+	case reflect.Struct:
+		return validateStruct(t, reflect.ValueOf(v))
+	default:
+		return fmt.Errorf("%w: the validated object is not a structure", e.ErrInternal)
+	}
+}
+
+func validateStruct(t reflect.Type, v reflect.Value) error {
+	errs := make(e.ValidationErrors, 0)
+	for i := 0; i < t.NumField(); i++ {
+		err := validateField(t.Field(i), v.Field(i))
+		if err != nil {
+			if errors.Is(err, e.ErrInternal) {
+				return fmt.Errorf("%w: field: %v", err, t.Field(i).Name)
+			}
+
+			errs = append(errs, e.ValidationError{
+				Field: t.Field(i).Name,
+				Err:   err,
+			})
+		}
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return errs
+}
+
+func validateField(field reflect.StructField, value reflect.Value) error {
+	switch field.Type.Kind() {
+	case reflect.Int, reflect.String:
+		return validateItem(field, value)
+
+	case reflect.Array, reflect.Slice:
+		errs := []error{}
+		for i := 0; i < value.Len(); i++ {
+			elem := value.Index(i)
+			err := validateItem(field, elem)
+			if err != nil {
+				if errors.Is(err, e.ErrInternal) {
+					return err
+				}
+				errs = append(errs, err)
+			}
+		}
+		if len(errs) == 0 {
+			return nil
+		}
+		return errors.Join(errs...)
+
+	default:
+		return e.ErrUnsupportedFieldType
+	}
+}
+
+func validateItem(field reflect.StructField, value reflect.Value) error {
+	rule := field.Tag.Get(constants.ValidateTag)
+	if len(rule) == 0 {
+		return nil
+	}
+	switch value.Kind() {
+	case reflect.Int:
+		return integer.Validate(int(value.Int()), rule)
+	case reflect.String:
+		return str.Validate(value.String(), rule)
+	default:
+		return e.ErrUnsupportedItemType
+	}
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"testing"
+
+	"github.com/antonT001/psychic-parakeet/hw09_struct_validator/e"
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -13,11 +16,11 @@ type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
 
 	App struct {
@@ -38,23 +41,59 @@ type (
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
+		name        string
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			name: "valid",
+			in: User{
+				ID:    "012345678901234567890123456789012345",
+				Name:  "Anton",
+				Age:   40,
+				Email: "anton@example.com",
+				Role:  "admin",
+				Phones: []string{
+					"89999999999",
+					"89111111111",
+				},
+				meta: []byte("qwertyui"),
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			name: "invalid len",
+			in: App{
+				Version: "123456",
+			},
+			expectedErr: e.VError("Version", errors.New("the value does not match the specified length")),
+		},
+		{
+			name: "no tag and unsupported value",
+			in: Token{
+				Header:    []byte("header"),
+				Payload:   []byte("payload"),
+				Signature: []byte("signature"),
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "not included integer and not validate tag",
+			in: Response{
+				Code: 1,
+				Body: "body",
+			},
+			expectedErr: e.VError("Code", errors.New("the value is not included in the specified set")),
+		},
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			tt := tt
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			t.Log(err)
+			require.Equal(t, tt.expectedErr, err)
 		})
 	}
 }
